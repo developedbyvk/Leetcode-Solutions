8784905c892390e5da1043c358c3501bd2932bbb
Vikas Kumar <113536720+developedbyvk@users.noreply.github.com>
2023-10-30 20:00:02 +0530

    Time: 13 ms (19.05%), Space: 10.4 MB (81.00%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,56 @@
+// class Solution {
+// public:
+//      static bool compareOnes(int a, int b) {
+//         int count_a = __builtin_popcount(a);  
+//         int count_b = __builtin_popcount(b); 
+    
+//         if (count_a == count_b) {
+//             return a < b;
+//         }
+        
+       
+//         return count_a < count_b;
+//     }
+
+//     vector<int> sortByBits(vector<int>& arr) {
+    
+//         sort(arr.begin(), arr.end(), compareOnes);
+//         return arr;
+//     }
+// };
+
+
+
 class Solution {
 public:
-     static bool compareOnes(int a, int b) {
-        int count_a = __builtin_popcount(a);  
-        int count_b = __builtin_popcount(b); 
     
+    int countOnes(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n & 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+        return count;
+    }
+
+    static bool compareOnes(int a, int b) {
+        Solution s;
+        int count_a = s.countOnes(a);
+        int count_b = s.countOnes(b);
+        
+   
         if (count_a == count_b) {
             return a < b;
         }
         
-       
+    
         return count_a < count_b;
     }
 
     vector<int> sortByBits(vector<int>& arr) {
-    
+   
         sort(arr.begin(), arr.end(), compareOnes);
         return arr;
     }
