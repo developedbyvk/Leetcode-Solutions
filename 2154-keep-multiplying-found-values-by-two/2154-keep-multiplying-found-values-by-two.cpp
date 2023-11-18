e6387e611b7f6bbac9f3c8d4afb4c8d4c8e08afa
Vikas Kumar <113536720+developedbyvk@users.noreply.github.com>
2023-11-19 00:43:53 +0530

    Time: 10 ms (25.40%), Space: 10.8 MB (52.77%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,29 +1,3 @@
-// class Solution {
-// public:
-//     int findFinalValue(vector<int>& nums, int original) {
-        
-//         int s=nums.size();
-        
-//         sort(nums.begin(),nums.end());
-        
-//         for(int i=0;i<s;i++){
-//             if(nums[i]==original){
-//                 original*=2;
-                
-//                 if(nums[i]<nums[s] && nums[i]==original){
-//                     if(original==nums[s]){
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
-        
-//         return original;
-        
-//     }
-// };
-
-
 #include <vector>
 #include <algorithm>
 
@@ -32,14 +6,14 @@ public:
     int findFinalValue(std::vector<int>& nums, int original) {
         int s = nums.size();
 
-        // Sort nums in ascending order
-        std::sort(nums.begin(), nums.end());
+
+        sort(nums.begin(), nums.end());
 
         for (int i = 0; i < s; i++) {
             if (nums[i] == original) {
                 original *= 2;
 
-                // Check if nums[i] is the last element and original is equal to nums[i]
+   
                 if (i == s - 1 && original == nums[i]) {
                     break;
                 }
