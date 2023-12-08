bb0f83413527e5e73dab36e10b5f2170ce0b38c4
Vikas Kumar <113536720+developedbyvk@users.noreply.github.com>
2023-12-08 23:48:31 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (90.75%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]!=' '){
+                int j=i;
+                while(s[j]!=' '){
+                    ans++;
+                    j--;
+                    if(j==-1) break;
+                }
+                break;
+            }
+        }
+        return ans;
+    }
+};
