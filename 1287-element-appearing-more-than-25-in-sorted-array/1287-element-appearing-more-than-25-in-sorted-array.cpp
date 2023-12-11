8390cedf18a3a572414bb52e163dc9ca83f325dc
Vikas Kumar <113536720+developedbyvk@users.noreply.github.com>
2023-12-12 00:07:08 +0530

    Time: 4 ms (95.90%), Space: 12.8 MB (57.06%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -4,18 +4,18 @@ class Solution {
 public:
     int findSpecialInteger(std::vector<int>& arr) {
         int n = arr.size();
-        int count = 1;  // Start count at 1 since the first element is considered.
-        int ans = arr[0];  // Initialize ans with the first element.
+        int count = 1;
+        int ans = arr[0];
 
         for (int i = 1; i < n; i++) {
             if (arr[i] == arr[i - 1]) {
                 count++;
                 if (count > 0.25 * n) {
                     ans = arr[i];
-                    break;  // Break out of the loop once the special integer is found.
+                    break;
                 }
             } else {
-                count = 1;  // Reset count for a new element.
+                count = 1;
             }
         }
 
