b724d9030542e7fe0f1c0190fdb6728242566a79
Vikas Kumar <113536720+developedbyvk@users.noreply.github.com>
2024-01-10 18:51:38 +0530

    Time: 11 ms (52.13%), Space: 11.5 MB (29.10%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -8,18 +8,18 @@ public:
     vector<string> findRelativeRanks(vector<int>& score) {
         vector<string> ans(score.size());
 
-        // Create a map to store the original indices of scores
+      
         unordered_map<int, int> scoreIndexMap;
 
-        // Populate the map with indices
+
         for (int i = 0; i < score.size(); i++) {
             scoreIndexMap[score[i]] = i;
         }
 
-        // Sort the scores in descending order
+     
         sort(score.rbegin(), score.rend());
 
-        // Assign medals based on the sorted order
+      
         for (int i = 0; i < score.size(); i++) {
             int originalIndex = scoreIndexMap[score[i]];
 
@@ -30,7 +30,7 @@ public:
             } else if (i == 2) {
                 ans[originalIndex] = "Bronze Medal";
             } else {
-                ans[originalIndex] = to_string(i + 1);  // Convert the rank to string
+                ans[originalIndex] = to_string(i + 1);  
             }
         }
 
